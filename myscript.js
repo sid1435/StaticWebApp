19366508f8098c24eb134b3d072347dcaf7f96f2
sid1435 <siddharth1435@gmail.com>
2025-02-12 11:23:08 +0530

    second commit

--- myscript.js
+++ myscript.js
@@ -1,3 +1,3 @@
 function showMessage() {
-    document.getElementById("message").innerText = "Hello From JavaScript!";
+    document.getElementById("message").innerText = "Hello From JavaScript! Updated";
 }
